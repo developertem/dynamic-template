cd8dd62237b23d757c66e50d24d41c17ff20379e
developertem <noorullah.nh@gmail.com>
2015-04-27 11:32:07 +0430

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,81 @@
+    <!-- Start Main Wrapper -->
+    <div id="mws-wrapper">
+    
+    	<!-- Necessary markup, do not remove -->
+		<div id="mws-sidebar-stitch"></div>
+		<div id="mws-sidebar-bg"></div>
+        
+        <!-- Sidebar Wrapper -->
+        <div id="mws-sidebar">
+        
+            <!-- Hidden Nav Collapse Button -->
+            <div id="mws-nav-collapse">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            
+        	<!-- Searchbox -->
+        	<div id="mws-searchbox" class="mws-inset">
+            	<form action="typography.html">
+                	<input type="text" class="mws-search-input" placeholder="Search...">
+                    <button type="submit" class="mws-search-submit"><i class="icon-search"></i></button>
+                </form>
+            </div>
+            
+            <!-- Main Navigation -->
+            <div id="mws-navigation">
+                <ul>
+                    <li class="active"><a href="#"><i class="icon-home"></i> Dashboard</a></li>
+                    <li>
+                        <a href="#"><i class="icon-list"></i>Bookcase</a>
+                        <ul>
+                            <li><a href="#">add bookcase</a></li>
+                            <li><a href="#">view bookcase</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon-list"></i>Echelon</a>
+                        <ul>
+                            <li><a href="#">add echelon</a></li>
+                            <li><a href="#">view echelon</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon-list"></i>part</a>
+                        <ul>
+                            <li><a href="#">add part</a></li>
+                            <li><a href="#">view part</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon-list"></i>Books</a>
+                        <ul>
+                            <li><a href="#">add books</a></li>
+                            <li><a href="#">view books</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon-list"></i>Members</a>
+                        <ul>
+                            <li><a href="#">add member</a></li>
+                            <li><a href="#">view members</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon-list"></i>Subject</a>
+                        <ul>
+                            <li><a href="#">add subject</a></li>
+                            <li><a href="#">view subjects</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>         
+        </div>
+                <!-- Main Container Start -->
+        <div id="mws-container" class="clearfix">
+        
+            <!-- Inner Container Start -->
+            <div class="container">
+            
